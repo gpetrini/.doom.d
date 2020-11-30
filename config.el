57810ff1a8a8195c042a27b00e0c69e328019aed
gpetrini <gpetrinidasilveira@gmail.com>
2020-11-30 10:03:11 -0300

    Trying to fix org-roam-server.

--- config.el
+++ config.el
@@ -281,6 +281,47 @@
          (("C-c n i" . org-roam-insert))
          (("C-c n I" . org-roam-insert-immediate))))
 
+(after! org-roam
+  (setq org-roam-graph-node-extra-config
+        '(("shape"      . "underline")
+          ("style"      . "rounded,filled")
+          ("fillcolor"  . "#EEEEEE")
+          ("color"      . "#C9C9C9")
+          ("fontcolor"  . "#111111")
+          ("fontname"   . "Overpass")))
+
+  (setq +org-roam-graph--html-template
+        (replace-regexp-in-string "%\\([^s]\\)" "%%\\1"
+                                  (f-read-text "~/.doom.d/misc/org-roam-template.html")))
+
+  (defadvice! +org-roam-graph--build-html (&optional node-query callback)
+    "Generate a graph showing the relations between nodes in NODE-QUERY. HTML style."
+    :override #'org-roam-graph--build
+    (unless (stringp org-roam-graph-executable)
+      (user-error "`org-roam-graph-executable' is not a string"))
+    (unless (executable-find org-roam-graph-executable)
+      (user-error (concat "Cannot find executable %s to generate the graph.  "
+                          "Please adjust `org-roam-graph-executable'")
+                  org-roam-graph-executable))
+    (let* ((node-query (or node-query
+                           `[:select [file titles] :from titles
+                             ,@(org-roam-graph--expand-matcher 'file t)]))
+           (graph      (org-roam-graph--dot node-query))
+           (temp-dot   (make-temp-file "graph." nil ".dot" graph))
+           (temp-graph (make-temp-file "graph." nil ".svg"))
+           (temp-html  (make-temp-file "graph." nil ".html")))
+      (org-roam-message "building graph")
+      (make-process
+       :name "*org-roam-graph--build-process*"
+       :buffer "*org-roam-graph--build-process*"
+       :command `(,org-roam-graph-executable ,temp-dot "-Tsvg" "-o" ,temp-graph)
+       :sentinel (progn
+                   (lambda (process _event)
+                     (when (= 0 (process-exit-status process))
+                       (write-region (format +org-roam-graph--html-template (f-read-text temp-graph)) nil temp-html)
+                       (when callback
+                         (funcall callback temp-html)))))))))
+
 (use-package! org-roam-bibtex
   :defer t
   :after org-roam
@@ -321,21 +362,21 @@ Time-stamp: %<%Y-%m-%d>
 
   )
 
-(use-package! org-roam-server
-  :defer t
+(use-package org-roam-server
+  :after (org-roam server)
   :config
   (setq org-roam-server-host "127.0.0.1"
-        org-roam-server-port 8080
-        org-roam-server-authenticate nil
+        org-roam-server-port 8078
         org-roam-server-export-inline-images t
-        org-roam-server-serve-files nil
-        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
-        org-roam-server-network-poll t
-        org-roam-server-network-arrows 'from
+        org-roam-server-authenticate nil
         org-roam-server-network-label-truncate t
         org-roam-server-network-label-truncate-length 60
-        org-roam-server-network-label-wrap-length 20))
-
+        org-roam-server-network-label-wrap-length 20)
+  (defun org-roam-server-open ()
+    "Ensure the server is active, then open the roam graph."
+    (interactive)
+    (org-roam-server-mode 1)
+    (browse-url-xdg-open (format "http://localhost:%d" org-roam-server-port))))
 (after! org-roam
   (unless (server-running-p)
     (smartparens-global-mode -1)
